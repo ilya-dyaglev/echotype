b1964e7c64c8ac2b737a39a02fff3f37436634e6
Ilya Dyaglev <idyaglev@amazon.com>
2024-11-07 21:37:46 +0000

    fixing routing

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { Amplify } from '@aws-amplify';
+import { Amplify } from 'aws-amplify';
 import outputs from '@/amplify_outputs.json';
 
 Amplify.configure(outputs);
